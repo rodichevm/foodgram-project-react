8269f08d49ff2bfb3608004c949d6bdf947f9300
Michael Rodichev <rodichevm@icloud.com>
2023-12-13 12:49:36 +0300

    fixed main.yml

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -107,7 +107,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = '/media/'
