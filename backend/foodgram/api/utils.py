ddf5c796b9f6f1477af7c409b9380a422d73bcf0
Michael Rodichev <rodichevm@icloud.com>
2023-12-23 10:44:36 +0300

    fixed utils.py

--- backend/foodgram/api/utils.py
+++ backend/foodgram/api/utils.py
@@ -1,26 +1,28 @@
 import datetime
 
 
-def send_message(data):
+def generate_shopping_cart_message(shopping_cart_list):
     result_lines = [
-        f'Дата: {datetime.date.today().isoformat()}\n',
+        f'Дата: {datetime.date.today().isoformat()}',
+        '',
         'ПРОДУКТЫ:',
         *[
             f'{index + 1}.'
-            f'{ingredient["ingredient__name"].capitalize()}'
-            f' - {ingredient["amount"]}'
-            f' {ingredient["ingredient__measurement_unit"]}'
-            for index, ingredient in enumerate(data)
+            f'{item["ingredient__name"].capitalize()}'
+            f' - {item["amount"]}'
+            f' {item["ingredient__measurement_unit"]}'
+            for index, item in enumerate(shopping_cart_list)
         ],
+        '',
         'РЕЦЕПТЫ:',
     ]
     added_recipes = set()
-    for index, recipe in enumerate(data):
-        if recipe["recipe__name"] not in added_recipes:
+    for index, item in enumerate(shopping_cart_list):
+        if item["recipe__name"] not in added_recipes:
             result_lines.append(
                 f'{index + 1}.'
-                f'{recipe["recipe__name"].capitalize()}'
+                f'{item["recipe__name"].capitalize()}'
             )
-            added_recipes.add(recipe["recipe__name"])
+            added_recipes.add(item["recipe__name"])
 
     return '\n'.join(result_lines)
